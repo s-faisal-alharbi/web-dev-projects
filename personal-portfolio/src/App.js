aaabb757f599435d5ea791ed2548f20687253686
s-faisal-alharbi <S.faisal.alharbi@gmail.com>
2024-01-09 12:11:10 +0300

    v0.69

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,27 +1,47 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from 'react'
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import LanguageSelector from './assets/components/LanguageSelector';
+
 import cookies from 'js-cookie'
+import { useTranslation } from 'react-i18next'
 // import { Hotels } from './components/Hotels';
 // import { HotelsGallery } from './components/HotelsGallery';
 
+const languages = [
+
+  {
+    code: 'en',
+    name: 'English',
+    country_code: 'gb',
+    dir:'ltr',
+  },
+  {
+    code: 'ar',
+    name: 'العربية',
+    dir: 'rtl',
+    country_code: 'sa',
+  },
+]
 
 
 
 function App() {
-  // const currentLanguageCode = cookies.get('i18next') || 'ar'
-  // const currentLanguage = languages.find((l) => l.code === currentLanguageCode)
-  // useEffect(() => {
-  //   console.log('Setting page stuff')
-  //   document.body.dir = currentLanguage.dir || 'rtl'
-  //   document.title = t('app_title')
-  // }, [currentLanguage, t])
+  const currentLanguageCode = cookies.get('i18next') || 'ar'
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode)
+  const { t } = useTranslation()
+  useEffect(() => {
+    console.log('Setting page stuff')
+    document.body.dir = currentLanguage.dir || 'rtl'
+    document.title = t('app_title')
+  }, [currentLanguage, t])
+
+ 
   return (
     <div className="App">
       <NavBar /> 
@@ -32,7 +52,6 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
-      <LanguageSelector />
     </div>
   );
 }
--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -1,35 +1,11 @@
 import React,{ useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.jpg';
-// import underline from '../assets/img/nabBottomBar.svg'
-// import navIcon1 from '../assets/img/nav-icon1.svg';
-// import navIcon2 from '../assets/img/nav-icon2.svg';
-// import navIcon3 from '../assets/img/nav-icon3.svg';
-// import { HashLink } from 'react-router-hash-link';
+import LanguageSelector from '../assets/components/LanguageSelector';
 import {BrowserRouter as Router} from "react-router-dom";
 import { useTranslation } from 'react-i18next';
-import i18n from '../assets/i18n';
-import i18next from "i18next";
 
-const LanguageSelector = () => {
-    
-  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
 
-  const chooseLanguage = (e) => {
-      e.preventDefault();
-      i18n.changeLanguage(e.target.value);   // i18n.changeLanguage() is used to change the language assigned to lng in i18n.js file.
-      setSelectedLanguage(e.target.value);
-      localStorage.setItem("lang", e.target.value);
-  }
-
-  return (
-      
-      <select defaultValue={selectedLanguage} onChange={chooseLanguage}>  
-          <option value="ar">عربي</option>
-          <option value="en">English</option>
-      </select>
-  );
-};
 export const NavBar = () => {
   const { t } = useTranslation()
   const [activeLink, setActiveLink] = useState('home');
@@ -64,34 +40,24 @@ export const NavBar = () => {
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className={t("nav_class1")}>
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>{t('Home')}</Nav.Link>
               <Nav.Link href="#hotels" className={activeLink === 'hotels' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('hotels')}>{t('Hotels')}</Nav.Link>
               
             </Nav>
-            <Nav className="ms-auto"> 
+            <Nav className={t("nav_class2")}> 
             <span className="navbar-text">
-            <Nav.Link href="#Hotels" className={activeLink === 'hotels' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('hotels')}>{t('Hotels')}</Nav.Link>   
+            {/* <Nav.Link href="#Hotels" className={activeLink === 'hotels' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('hotels')}>{t('Hotels')}</Nav.Link>    */}
             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>{t('Projects')}</Nav.Link>
-              {/* <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
-              </div> */}
-              {/* <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink> */}
-              {/* <label className="switch" onClick => {()=>i18next.changeLanguage(lng_code)} >
-                <input type="checkbox"/>
-                <span className="slider"/>
-              </label> */}
+              
+              <div className="switch">
+              <LanguageSelector />
+              </div>
             </span>
             </Nav>
           </Navbar.Collapse>
         </Container>
-
       </Navbar>
-      
     </Router>
   )
 }
